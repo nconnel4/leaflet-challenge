6376e4a7ed420a290a4ff91eed3091c7ffab0a69
nconnel4 <nconnel4@hotmail.com>
2021-07-29 08:15:20 -0400

    Add layer group for tectonic plates and earthquake data

--- Leaflet-Step-2/static/js/logic.js
+++ Leaflet-Step-2/static/js/logic.js
@@ -1,35 +1,44 @@
+// instatiate map
 const map = L.map('mapid', {
     center: [28.4449, -43.6699],
     zoom: 2.5
-  });
+});
 
-const addTileLayer = () => {
-    L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+// instantiate layer control
+layerControl = L.control.layers().addTo(map);
+
+
+const addTileLayer = (overlayName, mapboxId) => {
+    return L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
         attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
         tileSize: 512,
         maxZoom: 18,
         zoomOffset: -1,
-        id: "mapbox/streets-v11",
+        id: `mapbox/${mapboxId}`,
         accessToken: API_KEY
-    }).addTo(map);
+    });
 };
 
 const getMarkerColor = value => {
-    if (value >= 90) 
+    if (value >= 90)
         return '#f90015';
     else if (value >= 70)
         return '#f55700';
     else if (value >= 50)
         return '#f1c100';
     else if (value >= 30)
-        return '#eaef00'
+        return '#eaef00';
     else if (value >= 10)
-        return '#b3ed00'
-    else 
-        return '#00e51e'
+        return '#b3ed00';
+    else
+        return '#00e51e';
 }
 
 const addEarthquakeData = () => {
+
+    //instantiate empty layer group
+    let earthquakeLayer = L.layerGroup().addTo(map);
+
     d3.json('static/data/all_week.geojson.json').then(data => {
 
         data.features.forEach(dataPoint => {
@@ -39,39 +48,59 @@ const addEarthquakeData = () => {
             const depth = dataPoint.geometry.coordinates[2];
             const time = new Date(dataPoint.properties.time * 1000);
 
-            var circle = L.circleMarker([lat, long],  {
-                radius:magnitude,
+            var circle = L.circleMarker([lat, long], {
+                radius: magnitude,
                 color: getMarkerColor(depth),
                 fillColor: getMarkerColor(depth),
-                fillOpacity: 0.5}).addTo(map);
+                fillOpacity: 0.5
+            })
 
-            circle.bindPopup('<b>Date/Time:</b>' + time + '<br>' + 
-            '<b>Coordinates: </b>[' + lat + ', ' + long + ']<br>' + 
-            '<b>Magnitude:</b>' + magnitude + '<br>' + 
-            '<b>Depth:</b>' + depth + 'km<br>')
-            
+            circle.bindPopup('<b>Date/Time:</b>' + time + '<br>' +
+                '<b>Coordinates: </b>[' + lat + ', ' + long + ']<br>' +
+                '<b>Magnitude:</b>' + magnitude + '<br>' +
+                '<b>Depth:</b>' + depth + 'km<br>')
+
+            // add marker to layer group
+            earthquakeLayer.addLayer(circle);
+
         })
+
+        // add layer group to control
+        layerControl.addOverlay(earthquakeLayer, 'Earthquake');
     })
 };
 
 const addTectonicBoundaries = () => {
-    d3.json('static/data/PB2002_plates.json').then(data => {
-        console.log(data)
 
-        L.geoJSON(data, {
-            style: function(feature) {
+    // instantiate empty layer group
+    let tectonicLayer = L.layerGroup().addTo(map);
+
+    function addDataToLayer(feature, layer) {
+        // function to add geoJson data to layer group
+        tectonicLayer.addLayer(layer);
+    }
+
+    d3.json('static/data/PB2002_plates.json').then(data => {
+        console.log(data);
+
+        tectonicPlates = L.geoJSON(data, {
+            onEachFeature: addDataToLayer,
+            style: function () {
                 return {
                     color: 'orange',
                     fillOpacity: 0,
                     weight: 1
                 }
             }
-        }).addTo(map);
+        });
+
+        // add tectonic plates to layer group
+        layerControl.addOverlay(tectonicLayer, 'Tectonic Boundaries');
     })
 };
 
 const addLegend = () => {
-    const legend = L.control({position: 'bottomright'})
+    const legend = L.control({position: 'bottomright'});
 
     legend.onAdd = () => {
         var div = L.DomUtil.create('div', 'info legend'),
@@ -80,22 +109,41 @@ const addLegend = () => {
         div.innerHTML += '<b>Depth (km)</b><br>'
 
         for (let i = 0; i < grades.length; i++) {
-             div.innerHTML +=
+            div.innerHTML +=
                 '<i style="background:' + getMarkerColor(grades[i] + 1) + '"></i> ' +
-                 grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');            
-             }
+                grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+        }
 
-         return div;
+        return div;
     };
 
-    legend.addTo(map);
+    return legend;
 }
 
 const init = () => {
-    addTileLayer();
+
+    // create layers
+    let streets = addTileLayer('Streets', 'streets-v11');
+    let satellite = addTileLayer('Satellite', 'satellite-streets-v11');
+    let light = addTileLayer('Light', 'light-v10');
+
+    // set default layer to satellite
+    satellite.addTo(map)
+
+    // add layers to control
+    layerControl.addBaseLayer(streets, 'Streets');
+    layerControl.addBaseLayer(satellite, 'Satellite');
+    layerControl.addBaseLayer(light, 'Light')
+
+    // load tectonic plate and earthquake markers into map
     addTectonicBoundaries();
-    addEarthquakeData();
-    addLegend();
+    addEarthquakeData()
+
+    // add legend to map
+    let legend = addLegend();
+    legend.addTo(map);
+
 };
 
+// run initialization
 init();
